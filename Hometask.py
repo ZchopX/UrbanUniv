9b23f809db2dc551006dd4b486e80503677ddb8d
ZchopX <165720694+ZchopX@users.noreply.github.com>
2024-10-31 17:33:14 +0300

    Update Hometask.py

--- Hometask.py
+++ Hometask.py
@@ -33,3 +33,71 @@ else:
     print(Journal)
     print('Вы вышли из программы. До свидания!')
 
+######################################
+### Practical task ###
+
+Journal = dict()
+scores = list()
+mean_score = list()
+names = set()
+
+question = str(input('Вы хотите внести оценки учеников в журнал? (Да/Нет) '))
+if question==('Да') or ('да') or ('Lf') or ('lf'):
+    while question==('Да') or ('да') or ('Lf') or ('lf'):
+        names.add(input('Введите имя ученика, чьи оценки вы желаете внести '))
+
+        question = input('Хотите добавить ещё одного ученика? (Да/Нет) ')
+        if question!=('Да') or ('да') or ('Lf') or ('lf'):
+            break
+        else:
+            continue
+
+    sort_names = sorted(names)
+    for i in range(len(sort_names)):
+        a = input(f'Введите оценки ученика (без запятых) '
+                                     f'{sort_names[i]}: ')
+        a = list(a)
+        mean = (sum(a)/len(a))
+        mean_score.append(mean)
+        Journal.update({sort_names[i]: mean_score[i]})
+
+else:
+    print('Вот средний балл учеников на данный момент\n',
+          Journal,
+          '\nВы вышли из программы. До свидания!')
+
+
+
+# question = str(input('Вы хотите внести оценки учеников в журнал? (Да/Нет) '))
+# if question==('Да'):
+#     while question==('Да'):
+#         names.add(input('Введите имена учеников, чьи оценки вы желаете внести '))
+#         sort_names = sorted(name)
+#         for i in len(sort_names):
+#             list.append ('Введите оценки ученика ', )
+#             scnd_question = input('Хотите добавить ещё одного ученика? (Да/Нет) ')
+#             question = (scnd_question)
+#
+#
+#
+#
+#
+#
+#
+#
+#         if question==('Нет'):
+#             print( '\nВот средний балл учеников на данный момент')
+#             print(Journal)
+#             print('Вы вышли из программы. До свидания!')
+#         elif question==('Да'):
+#             print('')
+#         else:
+#             print('\nВот средний балл учеников на данный момент')
+#             print(Journal)
+#             print('Вы вышли из программы. До свидания!')
+#
+#
+# else:
+#     print('Вот средний балл учеников на данный момент')
+#     print(Journal)
+#     print('Вы вышли из программы. До свидания!')
